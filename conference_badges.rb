11ae2fa5aee39fdaa8808da0a9b411817cd57780
Jeremy Hill <j.c.hillmail@gmail.com>
2019-07-21 01:40:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+  return  "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(array)
+  greeting = []
+  array.each do |name|
+    greeting.push "Hello, my name is #{name}."
+  end
+    return greeting
+end  
+
+def assign_rooms(array)
+  messages = []
+  rooms = 1 
+  array.each do |names|
+    messages.push "Hello, #{names}! You'll be assigned to room #{rooms}!"
+    rooms += 1
+  end
+  return messages
+end 
+
+def printer
+  batch_badge_creator.each do |badge|
+    return badge
+  end 
+  assign_rooms.each do |pads|
+    return pads
+  end 
+end
